dee4b1e50a055f9bc51196fbc80aa93818a8f9c1
jordancaraballo <jordan.a.caraballo-vega@nasa.gov>
2023-02-07 11:53:19 -0500

    Adding docker and metrics

--- pytorch_caney/model/metrics.py
+++ pytorch_caney/model/metrics.py
@@ -1,4 +1,7 @@
 import logging
+from typing import List
+
+import torch
 import numpy as np
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import precision_score
@@ -17,6 +20,8 @@ __status__ = "Production"
 # ---------------------------------------------------------------------------
 # Module Methods
 # ---------------------------------------------------------------------------
+
+EPSILON = 1e-15
 
 
 # ------------------------------ Metric Functions -------------------------- #
@@ -40,6 +45,31 @@ def prec_val(y_true, y_pred):
 def recall_val(y_true, y_pred):
     return recall_score(y_true, y_pred, average='macro'), \
         recall_score(y_true, y_pred, average=None)
+
+
+def find_average(outputs: List, name: str) -> torch.Tensor:
+    if len(outputs[0][name].shape) == 0:
+        return torch.stack([x[name] for x in outputs]).mean()
+    return torch.cat([x[name] for x in outputs]).mean()
+
+
+def binary_mean_iou(
+            logits: torch.Tensor,
+            targets: torch.Tensor
+        ) -> torch.Tensor:
+
+    output = (logits > 0).int()
+
+    if output.shape != targets.shape:
+        targets = torch.squeeze(targets, 1)
+
+    intersection = (targets * output).sum()
+
+    union = targets.sum() + output.sum() - intersection
+
+    result = (intersection + EPSILON) / (union + EPSILON)
+
+    return result
 
 
 # -------------------------------------------------------------------------------
